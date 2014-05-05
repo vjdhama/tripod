9989990db5aa1ce3686dfab50134f1a34e8a50e0
Vijay Dhama <vjdhama26@gmail.com>
2014-05-05 19:41:34 +0530

    Added test cases for sign up

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+	test "user should enter a first name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+	end
 
+	test "user should enter a last name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end
+
+	test "user should enter a profile name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have a unique profile name" do
+		user = User.new
+		user.profile_name = users(:test).profile_name
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have profile name without spaces" do
+		user = User.new
+		user.profile_name = "My name"
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
 end
